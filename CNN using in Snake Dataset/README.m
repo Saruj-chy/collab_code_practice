b295d5c7c5eddd860f52b3792b173e0a161959f2
Saruj-chy <saruj.chy222@gmail.com>
2023-03-17 13:51:05 +0600

    readme add of CNN

---
+++ CNN using in Snake Dataset/README.m
@@ -0,0 +1,10 @@
+# Snake Image Identification Using CNN
+This project is modified from [CAT & DOG Dataset](https://www.kaggle.com/code/serkanpeldek/keras-cnn-transfer-learnings-on-cats-dogs-dataset), which is used by CNN model in Keras.
+
+### Quickstart
+Mounted with google drive for accessing the data: 
+
+```
+from google.colab import drive
+drive.mount('/content/drive')
+```
